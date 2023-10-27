0057b42a515adb698fca0ddffcb17a32d3314d98
Atif Aiman <alserembani94@gmail.com>
2023-10-27 20:34:16 +0800

    fix: authors to author

--- src/mocks/books.js
+++ src/mocks/books.js
@@ -4,84 +4,84 @@ const books = [
     "title": "Pride and Prejudice",
     "genre": "fiction",
     "publish_year": 1602,
-    "authors": "Jane Austen"
+    "author": "Jane Austen"
   },
   {
     "id": "alicesadventures0000lewi_h0b9",
     "title": "Alice's Adventures in Wonderland",
     "genre": "fiction",
     "publish_year": 1865,
-    "authors": "Lewis Carroll"
+    "author": "Lewis Carroll"
   },
   {
     "id": "huckleberryfinn0000unse_r2u4",
     "title": "Adventures of Huckleberry Finn",
     "genre": "fiction",
     "publish_year": 1707,
-    "authors": "Mark Twain"
+    "author": "Mark Twain"
   },
   {
     "id": "emmanovelinthree131aust",
     "title": "Emma",
     "genre": "fiction",
     "publish_year": 1815,
-    "authors": "Jane Austen"
+    "author": "Jane Austen"
   },
   {
     "id": "ghostseer01schiuoft",
     "title": "Frankenstein; or, The Modern Prometheus",
     "genre": "fiction",
     "publish_year": 1818,
-    "authors": "Mary Wollstonecraft Shelley"
+    "author": "Mary Wollstonecraft Shelley"
   },
   {
     "id": "nybc207238",
     "title": "The Picture of Dorian Gray",
     "genre": "fiction",
     "publish_year": 1890,
-    "authors": "Oscar Wilde"
+    "author": "Oscar Wilde"
   },
   {
     "id": "wutheringheights0000emil_z8q3",
     "title": "Wuthering Heights",
     "genre": "fiction",
     "publish_year": 1847,
-    "authors": "Emily Brontë"
+    "author": "Emily Brontë"
   },
   {
     "id": "works0000stev_b2w5",
     "title": "Treasure Island",
     "genre": "fiction",
     "publish_year": 1880,
-    "authors": "Robert Louis Stevenson"
+    "author": "Robert Louis Stevenson"
   },
   {
     "id": "sensesensibility0000jane_c2k7",
     "title": "Sense and Sensibility",
     "genre": "fiction",
     "publish_year": 1811,
-    "authors": "Jane Austen"
+    "author": "Jane Austen"
   },
   {
     "id": "littlewomenormeg00alcoiala",
     "title": "Little Women",
     "genre": "fiction",
     "publish_year": 1848,
-    "authors": "Louisa May Alcott"
+    "author": "Louisa May Alcott"
   },
   {
     "id": "worksdic23dickuoft",
     "title": "A Tale of Two Cities",
     "genre": "fiction",
     "publish_year": 1800,
-    "authors": "Charles Dickens"
+    "author": "Charles Dickens"
   },
   {
     "id": "1843christmascar00dickuoft",
     "title": "A Christmas Carol",
     "genre": "fiction",
     "publish_year": 1843,
-    "authors": "Charles Dickens"
+    "author": "Charles Dickens"
   }
 ];
 
